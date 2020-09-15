5a9ba58e6f240b1e2e77b4017eacf88b7c1c9253
rxrx3 <70162537+rxrx3@users.noreply.github.com>
2020-09-15 14:06:16 -0400

    Update marte.py

--- userbot/plugins/marte.py
+++ userbot/plugins/marte.py
@@ -1,82 +1,163 @@
-"""
-
-Available Commands:
-
-.sux
-
-.fuk
-
-.kiss"""
-
 from telethon import events
-
 import asyncio
+from uniborg.util import admin_cmd
 
-from userbot.utils import admin_cmd
-
-
-
-@borg.on(admin_cmd("marte"))
-
+@borg.on(admin_cmd(pattern=r"marte"))
 async def _(event):
-
     if event.fwd_from:
-
         return
-
-    animation_interval = 0,1
-
+    animation_interval = 0.1
     animation_ttl = range(0, 549755813888)
+ 
 
-    #input_str = event.pattern_match.group(1)
-
-   # if input_str == "marte":
-
-    await event.edit("🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵")
-
+    await event.edit("marte")
     animation_chars = [
-
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵",
-
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵",
-
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵",
-
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-            
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵",
-            
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-            
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵",
-            
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-            
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵",
-            
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-            
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵",
-            
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-            
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵",
-            
-            "🔴🔵 MARTE PICCOLA KAWAII 🔵🔴",
-            
-            "🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵"
-        ]
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",    
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            "`🔴🔵 MARTE PICCOLA KAWAII 🔵🔴`",
+            "`🔵🔴 **MARTE PICCOLA KAWAII** 🔴🔵`",
+            ]
 
     for i in animation_ttl:
-
         await asyncio.sleep(animation_interval)
-
         await event.edit(animation_chars[i % 549755813888])
-
-
